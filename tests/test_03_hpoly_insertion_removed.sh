e9fa70e04af00a2fe3bc660f2ca7244363e3671b
Laubscher <florian.laubscher@outlook.com>
2026-02-06 13:17:21 +0100

    fix test_03 (still passing)

--- tests/test_03_hpoly_insertion_removed.sh
+++ tests/test_03_hpoly_insertion_removed.sh
@@ -15,14 +15,13 @@ cat > "test_03_in.vcf" <<'VCF'
 ##INFO=<ID=DP4,Number=4,Type=Integer,Description="Counts for ref-fwd, ref-rev, alt-fwd, alt-rev">
 ##INFO=<ID=HRUN,Number=1,Type=Integer,Description="Homopolymer run length">
 #CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO
-UL23-HHV1	103	.	C	CA	0	.	DP=7996;AF=0.35;SB=0;DP4=3000,2180,1400,1416;INDEL;HRUN=6
+UL23-HHV1	103	.	CA	C	0	.	DP=7996;AF=0.35;SB=0;DP4=3000,2180,1400,1416;INDEL;HRUN=8
 VCF
 
-# on lance TON filtre
 python3 "$SCRIPT_DIR/../src/readVCF.py" "test_03_in.vcf" > "test_03_out.vcf"
 
 # La ligne variant ne doit plus être là
-if grep -q $'\t100\t' "test_03_out.vcf"; then
+if grep -q $'UL23-HHV1\t103\t.\tCA\tC' "test_03_out.vcf"; then
   echo "FAIL: homopolymer insertion (AF=0.35, HRUN=8) not removed"
   exit 1
 fi
